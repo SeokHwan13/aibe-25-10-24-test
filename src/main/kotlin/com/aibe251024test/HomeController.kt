3c4ffc2adac4bff18572d7a60ee9e695c3bdf6c7
SeokHwan13 <seokhwan1113@naver.com>
2025-10-27 15:13:25 +0900

    commit22

--- src/main/kotlin/com/aibe251024test/HomeController.kt
+++ src/main/kotlin/com/aibe251024test/HomeController.kt
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController
 class HomeController {
     @GetMapping("/")
     fun main(): String {
-        return "Hello HSH!!!"
+        return "Hello HSH!!aa!"
     }
 }
